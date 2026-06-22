ef1ef486394be0e1314757f5df5c53f0113c9566
Sreeleka <sreelekamanickam@gmail.com>
2026-06-22 13:48:04 +0530

    Fix Streamlit Cloud database connection

--- src/database/db_connection.py
+++ src/database/db_connection.py
@@ -1,23 +1,10 @@
 from sqlalchemy import create_engine
 import streamlit as st
-from urllib.parse import quote_plus
 
-# Read values from Streamlit Secrets
-DB_HOST = st.secrets["DB_HOST"]
-DB_PORT = st.secrets["DB_PORT"]
-DB_NAME = st.secrets["DB_NAME"]
-DB_USER = st.secrets["DB_USER"]
-DB_PASSWORD = st.secrets["DB_PASSWORD"]
-
-# Encode password safely
-password = quote_plus(DB_PASSWORD)
-
-# Create connection
 engine = create_engine(
-    f"postgresql://{DB_USER}:{password}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    st.secrets["DATABASE_URL"]
 )
 
-# Test connection
 try:
     conn = engine.connect()
     print("Database Connected Successfully!")
