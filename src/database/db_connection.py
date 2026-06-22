ffc2e23e771397ad54e2aefbb8ea4ec0fa7dd56c
Sreeleka <sreelekamanickam@gmail.com>
2026-06-22 14:05:01 +0530

    Fix Supabase connection

--- src/database/db_connection.py
+++ src/database/db_connection.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 import streamlit as st
+from urllib.parse import quote_plus
+
+password = quote_plus(
+    st.secrets["DB_PASSWORD"]
+)
 
 engine = create_engine(
-    st.secrets["DATABASE_URL"]
+    f"postgresql://{st.secrets['DB_USER']}:{password}@"
+    f"{st.secrets['DB_HOST']}:{st.secrets['DB_PORT']}/"
+    f"{st.secrets['DB_NAME']}"
 )
 
 try:
